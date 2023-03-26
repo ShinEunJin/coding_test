9737d7331a96b0a19319f756ad0046279bc86b66
ShinEunJin <sineun5501@naver.com>
2023-03-26 19:09:43 +0900

    크루스칼 union and find

---
+++ src/algorithm/unionAndFind.js
@@ -0,0 +1,24 @@
+const v = 5; // 노드의 수
+
+const node = Array.from({ length: v + 1 }, (_, i) => i);
+
+function getParent(n) {
+  // node[n]을 부모(루트) 노드로 바꾸고 뒤 결과값을 반환
+  // node[n]을 초기화 해줘야 하는거 주의
+  if (n === node[n]) return n;
+  return (node[n] = getParent(node[n]));
+}
+
+function unionNode(a, b) {
+  const nodeA = getParent(a);
+  const nodeB = getParent(b);
+  if (nodeA < nodeB) node[nodeB] = nodeA;
+  else node[nodeA] = nodeB;
+}
+
+//find는 굳이 함수를 사용할 필요가 없다.
+
+// if(getParent(a) !== getParent(b)){
+//     '할거 하기'
+//     unionNode(a, b)
+// }
